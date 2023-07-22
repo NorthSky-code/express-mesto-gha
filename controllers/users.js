e836b7b7612c510bb46de2195b11a59823e615c6
Evgeniy Fedotov <NorthSky-Evg@yandex.ru>
2023-07-22 11:21:46 +0300

    На ревью

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,7 @@ const createUser = (req, res, next) => {
         }
         return User.create({ name, about, avatar, email, password: hash })
           .then(() => {
-            res.status(201).send({ message: `Пользователь успешно зарегистрирован` });
+            res.status(201).send({ message: `Пользователь зарегистрирован` });
           })
           .catch((err) => {
             if (err.name === 'ValidationError') {
